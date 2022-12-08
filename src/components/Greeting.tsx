f19b62ae281c5eaf51fee33525832ff993e99ce9
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-12-08 10:57:33 -0800

    Updated background information

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -4,7 +4,7 @@ function Greeting() {
   return (
     <div className='greeting-wrapper'>
         <h2>Hi Everyone! Welcome to the game!</h2>
-        <p><b>Background:</b> This Web Application fetches data from The Open Trivia Database (<em>opentdb.com</em>). It provides a completely free JSON API that can be used without an API Key, all that needs to be done is to generate a URL and use it to retrieve trivia questions from the site.</p>
+        <p><b>Background:</b> This Web Application fetches data from The Open Trivia Database (<em>opentdb.com</em>). According to their site, they provide over 4,000 verified questions and over 9,500 pending questions to choose from.</p>
         <p><b>Instructions:</b> For those that haven never played this game before, the objective is quite simple. After clicking start you will be provided with 10 questions and multiple answers to choose from. All you have to do is select the one you think is right. The correct answer will be highlighted (along with the incorrect one should you choose that) and there is a scoreboard to keep track of how many answers you've successfully picked. Good luck and have fun :)</p>
     </div>
   )
